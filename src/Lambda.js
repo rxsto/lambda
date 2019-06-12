020df21527a36453087645c54ab2178ee8d1c9a1
Oskar <oskar.risto.lang@gmail.com>
2019-06-13 01:33:47 +0200

    Added Database support

--- src/Lambda.js
+++ src/Lambda.js
@@ -1,15 +1,31 @@
 const Discord = require('discord.js');
 const Logger = require('./core/util/Logger');
+const Database = require('./core/io/Database');
 const EventHandler = require('./core/events/EventHandler');
 const CommandHandler = require('./core/commands/CommandHandler');
+const Cache = require('./core/cache/Cache');
 
 class Lambda extends Discord.Client {
 
   constructor() {
     super();
+
+    this.defaultPrefix = process.env.PREFIX;
+
     this.log = new Logger();
+    this.database = new Database(this);
     this.eventHandler = new EventHandler(this);
     this.commandHandler = new CommandHandler(this);
+    this.userCache = new Cache(this);
+    this.guildCache = new Cache(this);
+    this.initialize();
+  }
+
+  async initialize() {
+    await this.database.connect();
+    await this.userCache.initialize(this.database.db(process.env.DB_DATABASE).collection('users'));
+    await this.guildCache.initialize(this.database.db(process.env.DB_DATABASE).collection('guilds'));
+    await this.login((process.env.DEBUG ? process.env.DISCORD_TOKEN_DEBUG : process.env.DISCORD_TOKEN)).then(this.log.info('[Login] Successfully logged into Discords API')).catch(error => this.log.error(error));
   }
 }
 
